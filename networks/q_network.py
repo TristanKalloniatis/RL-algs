e83c5893f1cd411441df9933859bab6f4013d3de
TristanKalloniatis <tristankalloniatis@gmail.com>
2021-03-29 15:02:12 +0100

    Image networks

--- networks/q_network.py
+++ networks/q_network.py
@@ -7,9 +7,9 @@ class DuellingDQN(torch.nn.Module):
     def __init__(self, env: Environment, hidden_size: int):
         super().__init__()
         self.feature_map = None
-        num_feature_maps = 1 + sum([env.has_goal, env.has_obstacle])
+        num_inputs = 1 + sum([env.has_goal, env.has_obstacle])
         self.combine_feature_map = torch.nn.Sequential(
-            torch.nn.Linear(num_feature_maps * hidden_size, hidden_size),
+            torch.nn.Linear(num_inputs * hidden_size, hidden_size),
             torch.nn.ReLU(),
         )
         self.value_map = torch.nn.Linear(hidden_size, 1)
@@ -33,8 +33,25 @@ class QNetwork(DuellingDQN):
 
 
 class ConvQNetwork(DuellingDQN):
-    def __init__(self, env: Environment, hidden_size: int):
+    def __init__(
+        self,
+        env: Environment,
+        hidden_size: int,
+        kernel_size: int = 3,
+        num_feature_maps: int = 2,
+    ):
+        assert kernel_size % 2 == 1, "For simplicity only support odd kernel sizes"
+        pad_size = (kernel_size - 1) / 2
         super().__init__(env, hidden_size)
         self.feature_map = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 2, 3, padding=1), torch.nn.ReLU(), torch.nn.Flatten()
-        )  # todo: finish this definition
+            torch.nn.Conv2d(1, num_feature_maps, kernel_size, padding=pad_size),
+            torch.nn.ReLU(),
+            torch.nn.Flatten(),
+            torch.nn.Linear(
+                num_feature_maps
+                * env.observation_space["obs"].shape[0]
+                * env.observation_space["obs"].shape[1],
+                hidden_size,
+            ),
+            torch.nn.ReLU(),
+        )
